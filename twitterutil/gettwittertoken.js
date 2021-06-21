173da0f9d1a72e52c1cc0e83845e2ebf4e36bce3
ferreting <monika.frs29@gmail.com>
2021-06-21 23:40:22 +0700

    add twitter function

--- twitterutil/gettwittertoken.js
+++ twitterutil/gettwittertoken.js
@@ -1,16 +1,92 @@
-const Twitter = require('twitter-v2');
 const needle = require('needle');
 
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?user.fields=username,profile_image_url,url&expansions=author_id,attachments.media_keys&media.fields=preview_image_url,url'
+
+const rules = [{value : '-is:retweet (from:natasha_mikha14 OR from:ryoukochikage OR from:NoyomiNeichi OR from:JoanaMaoh OR from:KotobaStation)'}]
+
+// get stream rules
+async function getTweetsRules() {
+    const response = await needle('get', rulesURL, {
+        headers: {
+            Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAK5oQwEAAAAAlh%2FWJL%2BDsrSNdxYZucVXmI5pUek%3DbZIAzdJtCsr3TII4h2N8WvOAtiUhQB7xwaAvsWBBYAYo3UjVp0',
+        }
+    })
+
+    console.log(response.body)
+    return response.body
+}
+
+
+function streamTweet(client) {
+    const stream = needle.get(streamURL, {
+        headers: {
+            Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAK5oQwEAAAAAlh%2FWJL%2BDsrSNdxYZucVXmI5pUek%3DbZIAzdJtCsr3TII4h2N8WvOAtiUhQB7xwaAvsWBBYAYo3UjVp0',
+        }
+    })
+
+    stream.on('data', (datas) => {
+        try {
+        //  console.log(media)      
+        //  console.log(JSON.stringify(json)) 
+          const poster = require('./posttweet');
+          poster.post(datas, client)
+        } catch (error) {
+            
+        }
+    })
+}
+
+// delete stream rules
+async function deleteTweetsRules(rules) {
+    if(!Array.isArray(rules.data)) {
+        return null
+    }
+
+    const ids = rules.data.map((rule) => rule.id)
+
+    const data = {
+        delete: {
+            ids: ids
+        }
+    }
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type' : 'application/json',
+            Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAK5oQwEAAAAAlh%2FWJL%2BDsrSNdxYZucVXmI5pUek%3DbZIAzdJtCsr3TII4h2N8WvOAtiUhQB7xwaAvsWBBYAYo3UjVp0',
+        }
+    })
+    return response.body
+}
+
+// post stream rules
+async function setTweetsRules() {
+    const data = {
+        add: rules
+    }
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type' : 'application/json',
+            Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAK5oQwEAAAAAlh%2FWJL%2BDsrSNdxYZucVXmI5pUek%3DbZIAzdJtCsr3TII4h2N8WvOAtiUhQB7xwaAvsWBBYAYo3UjVp0',
+        }
+    }) 
+    return response.body
+}
+
 module.exports = {   
-    run : async() => {
-        const client = new Twitter ({
-            consumer_key: 'pgb3CLQokmQRLgqIB6HVtax6x',
-            consumer_secret: 'wI9w9DCA40xWlLLf3WmXQSb4vqz6mn44a2NNpGB1nw8L2jscXw',
-            access_token_key: '1395936938637467651-dndLSLJGMCWWFnmmzJOKUwQNkcK4Yo',
-            access_token_secret: 'DCmCDyKluUBuwY1RYAPlvbkaUlvMWbWo4pUOMOEhUhZIM',       
-        });
-        const { data } = await client.get('tweets', { ids: '1395936938637467651' });
-        console.log(data);
+    run : async(client) => {
+        console.log(client.user.username)
+        try {
+            currentRules = await getTweetsRules()
+            await deleteTweetsRules(currentRules)
+            await setTweetsRules()
+        } catch (error) {
+            console.error(error)
+            process.exit(1)
+        }
+
+        streamTweet(client)
     }
     
 }
