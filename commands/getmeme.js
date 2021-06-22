859b01cc9356ffd3e007f7b95c71dd4032532860
ferreting <monika.frs29@gmail.com>
2021-06-23 00:44:30 +0700

    add meme functional

---
+++ commands/getmeme.js
@@ -0,0 +1,11 @@
+const needle = require('needle')
+module.exports = {
+    name: 'get twitter meme',
+    description : 'post twitter meme to desired channel',
+    searchMeme: async(message) => {
+        const url = 'https://meme-api.herokuapp.com/gimme';
+        const meme = await needle('get', url);
+        const memeImage = meme.body.preview[3];
+        message.channel.send('Here\'s a meme for ya. LMAO', {files : [memeImage]})
+    }
+}
