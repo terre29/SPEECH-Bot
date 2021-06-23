0260c5085c07a980f4f9c9d48a27ecb1e0a14fdd
ferreting <monika.frs29@gmail.com>
2021-06-23 18:53:26 +0700

    add kotoba member

---
+++ kotobamember/natasha.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name : 'Natasha Mikhaylova',
+    descripton : 'A Russian soldier who serve his country. Very loyal and patriotic toward Russia',
+    occupation : 'Soldier',
+    palletcolor: '#ED2628',
+    twitteruser : '@natasha_mikha14',
+    twitterurl : 'https://twitter.com/natasha_mikha14',
+} 
