52bcfec89fb7d6d3401bc03828bf6f4d88bda65b
ferreting <monika.frs29@gmail.com>
2021-06-23 14:46:47 +0700

    meme bug fix

--- commands/getmeme.js
+++ commands/getmeme.js
@@ -2,10 +2,14 @@ const needle = require('needle')
 module.exports = {
     name: 'get twitter meme',
     description : 'post twitter meme to desired channel',
-    searchMeme: async(message) => {
+    searchMeme: async (message) => {
         const url = 'https://meme-api.herokuapp.com/gimme';
-        const meme = await needle('get', url);
+        const meme = await needle('get', (url), {
+
+        });
+        console.log(meme.body)
         const memeImage = meme.body.preview[3];
         message.channel.send('Here\'s a meme for ya. LMAO', {files : [memeImage]})
+        
     }
 }
